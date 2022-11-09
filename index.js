683266b03555482c3eab60dc2e7a47c02cb23914
Mehedi Hasan <you@example.com>
2022-11-09 13:34:02 +0600

    the server is initialized

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('the server is running');
+})
+
+app.listen(port, () =>{
+    console.log(`the server is running on ${port}`);
+})
