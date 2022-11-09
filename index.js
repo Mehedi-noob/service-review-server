2303d25d4998ebb2a56bb53cb68d998d116e3ad4
Mehedi Hasan <you@example.com>
2022-11-09 17:33:33 +0600

    services data called

--- index.js
+++ index.js
@@ -15,17 +15,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rh6ekch.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        const serviceCollection = client.db('SNservice').collection('services');
+
+        app.get('/services', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error));
 // mongodb end
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('the server is running');
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`the server is running on ${port}`);
 })
