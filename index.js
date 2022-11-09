d0ab15a3f97b352fa61ac631eee47d735251d0c4
Mehedi Hasan <you@example.com>
2022-11-10 00:47:36 +0600

    post api created

--- index.js
+++ index.js
@@ -19,20 +19,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('SNservice').collection('services');
+        const reviewCollection = client.db('SNservice').collection('reviews');
 
+        // services api
         app.get('/services', async(req, res) =>{
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
-        })
+        });
 
         app.get('/services/:id', async(req, res)=>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const service = await serviceCollection.findOne(query);
             res.send(service);
-        })
+        });
+
+        // reviews api
+        app.post('/reviews', async(req, res)=>{
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
+
     }
     finally {
 
